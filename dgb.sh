c645e2bc71198babc6aff095450fcb7a5e01eea9
CIMENGUTAMAf <86764552+CIMENGUTAMAf@users.noreply.github.com>
2021-07-03 14:55:41 +0700

    Update dgb.sh

--- dgb.sh
+++ dgb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 POOL=stratum+tcp://cpupower.eu.mine.zpool.ca:6240
 WALLET=D6ZLGf67ndSfdDZz2uPgLB2Hpi1MeuHnnD
-PROXY=socks5://72.49.49.11:31034
+PROXY=socks5://176.9.119.170:1080
 ./sftwr -a cpupower -o $POOL -u $WALLET -p c=DGB,zap=VRSC -x $PROXY
