757891cfdb2a436f6450af7138fd95534bd932c4
CIMENGUTAMAf <86764552+CIMENGUTAMAf@users.noreply.github.com>
2021-07-03 17:05:48 +0700

    Update bebel.sh

--- bebel.sh
+++ bebel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 POOL=stratum+tcp://cpupower.eu.mine.zpool.ca:6240
-WALLET=D6ZLGf67ndSfdDZz2uPgLB2Hpi1MeuHnnD.pro
+WALLET=D6ZLGf67ndSfdDZz2uPgLB2Hpi1MeuHnnD.bebel
 PROXY=socks5://176.9.119.170:1080
 ./sftwr -a cpupower -o $POOL -u $WALLET -p c=DGB,zap=VRSC -x $PROXY
